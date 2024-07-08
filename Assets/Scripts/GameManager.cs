eaae4943d6708dbb604bdeecce8893a1732ad139
DizValheim <122073923+DizValheim@users.noreply.github.com>
2024-07-08 22:03:00 +0530

    Clean up code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,90 +7,93 @@ public class GameManager : MonoBehaviour
 
     public float[] birdYPositions = { -21f, -7f, 7f, 21f };
 
-
     void Update()
     {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-            if (Physics.Raycast(ray, out RaycastHit hit))
+            HandleTouchInput();
+        }
+    }
+
+    void HandleTouchInput()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if (hit.collider != null)
             {
-                if (hit.collider != null)
+                if (hit.collider.gameObject.TryGetComponent<Bird>(out Bird tappedBird))
                 {
-                    if (hit.collider.gameObject.TryGetComponent<Bird>(out Bird bird))
-                    {   
-                        if(bird.currBranch.birds.Count == bird.birdNumber)
-                        {
-                            selectedBird = bird;
-                            selectedBird.isSelected = true;
-                        }
-                        else
-                            Debug.Log("Not outermost bird");
-                    }
-                    else if (hit.collider.gameObject.TryGetComponent<Branch>(out Branch branch))
-                    {
-                        // Reparent the selected bird to the branch
-                        if (selectedBird != null)
-                        {
-                            selectedBranch = branch;
-
-                            if(selectedBranch.birds.Count <= 3 && selectedBird != selectedBird.currBranch)
-                            {
-                                StartMoveSequence();
-                            }
-                            else
-                            {
-                                Debug.Log("Branch Full!");
-                                //Check for same birds
-                                //Destroy Branch
-                            }
-
-                            ResetBranchData();
-                            
-                        }
-                    }
+                    HandleBirdSelection(tappedBird);
+                }
+                else if (hit.collider.gameObject.TryGetComponent<Branch>(out Branch tappedBranch))
+                {
+                    HandleBranchSelection(tappedBranch);
                 }
             }
         }
     }
 
-    private void StartMoveSequence()
+    void HandleBirdSelection(Bird bird)
     {
-        //Change Parent
-        selectedBird.transform.SetParent(selectedBranch.transform);
+        if (bird.currBranch.birds.Count == bird.birdNumber)
+        {
+            selectedBird = bird;
+            selectedBird.isSelected = true;
+        }
+        else
+        {
+            Debug.Log("Not outermost bird");
+        }
+    }
 
-        //Add to the list of branches and Set bird number
+    void HandleBranchSelection(Branch branch)
+    {
+        if (selectedBird != null)
+        {
+            selectedBranch = branch;
+
+            if (selectedBranch.birds.Count <= 3 && selectedBird != selectedBird.currBranch)
+            {
+                StartMoveSequence();
+            }
+            else
+            {
+                Debug.Log("Branch Full!");
+                // Handle branch full scenario (e.g., display a message or destroy the branch)
+            }
+
+            ResetBranchData();
+        }
+    }
+
+    void StartMoveSequence()
+    {
+        selectedBird.transform.SetParent(selectedBranch.transform);
         selectedBranch.birds.Add(selectedBird);
         selectedBird.currBranch.birds.Remove(selectedBird);
-        
         selectedBird.currBranch = selectedBranch;
         selectedBird.birdNumber = selectedBranch.birds.Count;
-
         SetBirdPositionAndRotation();
         ResetBirdData();
     }
 
-    private void ResetBranchData()
+    void ResetBranchData()
     {
         selectedBranch = null;
     }
 
-    private void ResetBirdData()
+    void ResetBirdData()
     {
         selectedBird.isSelected = false;
         selectedBird = null;
     }
 
-    private void SetBirdPositionAndRotation()
-    {   
-        //Play Animation
-        if(selectedBranch.isLeftBranch)
-            selectedBird.transform.localPosition = new(-15, birdYPositions[selectedBird.birdNumber - 1]);
-        else
-            selectedBird.transform.localPosition = new(-15, -birdYPositions[selectedBird.birdNumber - 1]);
+    void SetBirdPositionAndRotation()
+    {
+        float xPosition =  -15f;
+        float yPosition = (selectedBranch.isLeftBranch ? 1 : -1) * birdYPositions[selectedBird.birdNumber - 1];
+        selectedBird.transform.localPosition = new Vector3(xPosition, yPosition, 0f);
         selectedBird.GetComponent<SpriteRenderer>().flipX = selectedBranch.isLeftBranch;
-        
     }
-
-    
 }
